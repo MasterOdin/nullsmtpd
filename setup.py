5070ec8b15cf487aa211ce90f959cb2bb232ef6a
Matthew Peveler <matt.peveler@gmail.com>
2017-04-12 17:29:12 -0400

    move log file to /var/log

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='nullsmtp',
-      version='0.1.0',
+      version='0.1.1',
       url='http://github.com/MasterOdin/nullsmtp',
       descriptoin='Fake SMTP server',
       long_description=open('README.rst').read(),
