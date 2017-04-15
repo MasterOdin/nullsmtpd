2226c1a152899dca37c0f41949f75f472a4f76ba
Matthew Peveler <matt.peveler@gmail.com>
2017-04-14 22:46:02 -0400

    remove pointless files

--- build/lib/nullsmtp/__init__.py
+++
@@ -1,8 +0,0 @@
-""" Defines an easy entry point for our application"""
-
-__author__ = 'Matthew Peveler'
-__version__ = '0.2.0'
-__license__ = 'Unlicense (Public Domain)'
-
-from nullsmtp.nullsmtp import main
-from nullsmtp.nullsmtp import NullSMTP
