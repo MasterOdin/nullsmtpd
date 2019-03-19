a93f5d5834b8e183e457535ee6c799437857f97a
Matthew Peveler <matt.peveler@gmail.com>
2019-03-19 13:00:42 -0400

    Disable installation to init.d

--- nullsmtpd/version.py
+++ nullsmtpd/version.py
@@ -3,5 +3,5 @@ Metadata about the project
 """
 
 __author__ = 'Matthew Peveler'
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 __license__ = 'Unlicense (Public Domain)'
--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ from nullsmtpd.version import __author__, __version__
 # If we're not running as root, or the /etc/init.d folder doesn't exist, don't bother copying
 # in our services file for NullSMTP
 # pylint: disable=invalid-name
-if pwd.getpwnam("root").pw_uid != os.getuid() or not os.path.isdir(os.path.join("etc", "init.d")):
-    data_files = []
-else:
-    data_files = [('/etc/init.d', ['init.d/nullsmtpd'])]
+#if pwd.getpwnam("root").pw_uid != os.getuid() or not os.path.isdir(os.path.join("etc", "init.d")):
+#    data_files = []
+#else:
+#    data_files = [('/etc/init.d', ['init.d/nullsmtpd'])]
 
 setup(name='nullsmtpd',
       version=__version__,
@@ -41,5 +41,5 @@ setup(name='nullsmtpd',
               'nullsmtpd = nullsmtpd.nullsmtpd:main',
           ]
       },
-      data_files=data_files,
+#      data_files=data_files,
       install_requires=['aiosmtpd'])
